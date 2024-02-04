f0c12e4ac5610465389e2a6c21169c081925ef71
nilj07 <111432313+nilj07@users.noreply.github.com>
2024-02-04 15:21:13 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,14 @@
+    // JavaScript function to redirect to a new page
+    function bollywood() {
+        // Change "newpage.html" to the actual file path or URL you want to redirect to
+        window.location.href = "bollywood.html";
+    }
+    function home(){
+        window.location.href = "index.html"
+    }
+    function Hollywood(){
+        window.location.href = "Hollywood.html";
+    }
+    function Movie_1(){
+        window.location.href = "bolly-movie-1.html";
+    }
